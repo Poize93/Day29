d73d4f3c997cd68defa52c3317a50eb31f9616dd
Poize93 <delhi93rsharma@gmail.com>
2022-03-21 23:16:12 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './style.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from 'react-router-dom';
 import User from './User';
 
 export default function RouterComponent() {
@@ -8,10 +14,9 @@ export default function RouterComponent() {
     <div>
       <BrowserRouter>
         <Link to="/">Dashboard</Link> &nbsp;
-        <Link to="/Contactus">ContactUS</Link>&nbsp;
+        <Link to="/Contactus">Contact US</Link>&nbsp;
         <Link to="/User/4/course/10?acvsf&df">User</Link>&nbsp;
         <Link to="/aboutus">About Us</Link> &nbsp;
-
         <Routes>
           <Route path="/" element={<Dashboard />}></Route>
           <Route path="/aboutus" element={<AboutUs />}></Route>
@@ -27,13 +32,32 @@ export default function RouterComponent() {
 }
 
 function Dashboard() {
-  return <h3>DashBoard </h3>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <h3>DashBoard </h3>
+      <button onClick={()=>navigate(-1)}>GoBack</button> &nbsp;
+      <button onClick={()=>navigate(1)}>Go Forward</button>
+    </>
+  );
 }
 
 function AboutUs() {
-  return <h3>AboutUs </h3>;
+  return (
+    <>
+      <h3>AboutUs </h3>
+      <button onClick={()=>navigate(-1)}>GoBack</button> &nbsp;
+      <button onClick={()=>navigate(1)}>Go Forward</button>
+    </>
+  );
 }
 
 function ContactUs() {
-  return <h3>ContactUs </h3>;
+  return (
+    <>
+      <h3>Contact Us </h3>
+      <button onClick={()=>navigate(-1)}>GoBack</button> &nbsp;
+      <button onClick={()=>navigate(1)}>Go Forward</button>
+    </>
+  );
 }
--- src/User.js
+++ src/User.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+
+
 export default function User() {
   const params = useParams(); // getting bac values from QueryParam
   console.log(params);
+  const navigate = useNavigate();
   return (
     <>
       <h3>User </h3>
+      <button onClick={()=>navigate(-1)}>GoBack</button> &nbsp;
+      <button onClick={()=>navigate(1)}>Go Forward</button>
     </>
   );
 }
