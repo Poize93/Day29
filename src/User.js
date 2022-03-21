b238e32140abc260d0533cb33d8640d95ccdaab2
Poize93 <delhi93rsharma@gmail.com>
2022-03-21 22:41:11 +0530

    1 file modified

--- src/User.js
+++ src/User.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import { useParams } from 'react-router-dom';
 export default function User() {
+  const params = useParams(); // getting bac values from QueryParam
+  console.log(params);
   return (
     <>
       <h3>User </h3>
